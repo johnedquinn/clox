2e6ac4e054ab090bdf86ac51c9781d7cefdae0af
John Quinn <johnedquinn@gmail.com>
2025-10-22 07:52:22 -0700

    Completed 14

--- chunk.h
+++ chunk.h
@@ -13,6 +13,7 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
     ValueArray constants;
 } Chunk;
 
@@ -20,7 +21,7 @@ void initChunk(Chunk* chunk);
 
 void freeChunk(Chunk* chunk);
 
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 
 int addConstant(Chunk* chunk, Value value);
 
