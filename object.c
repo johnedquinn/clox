94640121f5371e8e12823760cd553d7a555f0a9c
John Quinn <johnedquinn@gmail.com>
2025-10-29 21:37:33 -0700

    Completed 20.4

--- object.c
+++ object.c
@@ -10,28 +10,55 @@
 #define ALLOCATE_OBJ(type, objectType) \
     (type*)allocateObject(sizeof(type), objectType)
 
+/**
+ * Allocates a new object of the given type.
+ */
 static Obj* allocateObject(size_t size, ObjType type) {
     Obj* object = (Obj*)reallocate(NULL, 0, size);
     object->type = type;
     return object;
 }
 
-static ObjString* allocateString(char* chars, int length) {
+/**
+ * Allocates a new ObjString.
+ */
+static ObjString* allocateString(char* chars, int length, uint32_t hash) {
     ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
+    string->hash = hash;
     return string;
 }
 
-ObjString* takeString(char* chars, int length) {
-    return allocateString(chars, length);
+/**
+ * FNV-1a hash function
+ */
+static uint32_t hashString(const char* key, int length) {
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++) {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
 }
 
+/**
+ * Takes ownership of the given char array.
+ */
+ObjString* takeString(char* chars, int length) {
+    uint32_t hash = hashString(chars, length);
+    return allocateString(chars, length, hash);
+}
+
+/**
+ * Copies the given char array.
+ */
 ObjString* copyString(const char* chars, int length) {
+    uint32_t hash = hashString(chars, length);
     char* heapChars = ALLOCATE(char, length + 1);
     memcpy(heapChars, chars, length);
     heapChars[length] = '\0';
-    return allocateString(heapChars, length);
+    return allocateString(heapChars, length, hash);
 }
 
 void printObject(Value value) {
